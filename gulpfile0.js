26c3298328a35606438ebfd0222126e7fb624301
bercly0b <vvworkmail@yandex.ru>
2018-10-21 18:46:13 +0300

    removed unused css

---
+++ gulpfile0.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat')
+const cleanCss = require('gulp-clean-css')
+const uglify = require('gulp-uglify-es').default
+const gutil = require('gulp-util')
+const connect = require('gulp-connect-multi')
+
+const devServer = connect()
+// const ngrok = connect()
+
+gulp.task('html', () => {
+  return gulp.src('src/index.html')
+    .pipe(devServer.reload())
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('script', () => {
+  return gulp.src('src/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()) })
+    .pipe(gulp.dest('public'))
+    .pipe(devServer.reload())
+})
+
+gulp.task('scss', () => {
+  return gulp.src('src/scss/**/*.scss')
+    // .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    // .pipe(sourcemaps.write())
+    .pipe(cleanCss())
+    .pipe(gulp.dest('public'))
+    .pipe(devServer.reload())
+})
+
+gulp.task('watchAll', () => {
+  gulp.watch('src/scss/**/*.scss', ['scss'])
+  gulp.watch('src/js/**/*.js', ['script'])
+  gulp.watch('src/index.html', ['html'])
+})
+
+
+gulp.task('connect', devServer.server({
+  root: ['public'],
+  port: 9999,
+  livereload: true,
+  open: { browser: 'Google Chrome' }
+}))
+
+// gulp.task('connectNgrok', ngrok.server({
+//   root: ['public'],
+//   port: 9999,
+//   livereload: false
+// }))
+
+
+gulp.task('default', ['connect', 'watchAll'])
+// gulp.task('default', ['connect', 'connectNgrok', 'watchAll'])
