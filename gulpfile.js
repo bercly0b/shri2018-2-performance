099cf27bb8e277e3c646f9286b26110df019e4bb
bercly0b <vvworkmail@yandex.ru>
2018-10-30 14:53:39 +0300

    minificated html and script

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ const gulp = require('gulp')
 const cleanCss = require('gulp-clean-css')
 const uglify = require('gulp-uglify-es').default
 const gutil = require('gulp-util')
+const htmlmin = require('gulp-htmlmin')
 
 gulp.task('html', () => {
   return gulp.src('src/index.html')
+    // .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('public'))
 })
 
 gulp.task('script', () => {
   return gulp.src('src/scripts.js')
-    .pipe(uglify())
+    .pipe(uglify({ mangle: { toplevel: true } }))
     .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()) })
     .pipe(gulp.dest('public'))
 })
